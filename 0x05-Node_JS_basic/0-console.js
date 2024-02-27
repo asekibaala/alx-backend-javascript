94449440b43089a6a58ad531acbfddc36797122b
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2024-02-27 19:58:43 +0300

    Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Abel Sekibaala
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
